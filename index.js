11750d342837ace783c5754f52d9854c61f971f0
Tyson Williams <tyson.j.williams@gmail.com>
2018-07-23 16:17:23 -0600

    Add get method to find users by id update route path to use '/api'

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ server.get("/", (req, res) => {
   res.send("<h1>Welcome to Node Express Mini</h1>");
 });
 
-server.get("/users", (req, res) => {
+server.get("/api/users", (req, res) => {
   const users = db.find();
   users
     .then(users => res.status(200).json(users));
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = db.findById(id);
+  user
+    .then(user => res.status(200).json(user));
 });
 
 // Server listening on port (const PORT) above
