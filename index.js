bd4b9ef40af32346a105146c7a1c6944001b3651
Tyson Williams <tyson.j.williams@gmail.com>
2018-07-23 16:03:14 -0600

    Update server import db update root '/' url get method to display 'Welcome to Node Express Mini' create get method at '/users' to find and display users

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
+const db = require("./data/db");
 
 const server = express();
+express.json();
 const PORT = 8000;
 
 // Middleware
@@ -9,8 +11,13 @@ server.use(helmet());
 
 // API endpoints
 server.get("/", (req, res) => {
-  // res.send("<h1>Hello World</h1>");
-  res.send({ hello: "world" });
+  res.send("<h1>Welcome to Node Express Mini</h1>");
+});
+
+server.get("/users", (req, res) => {
+  const users = db.find();
+  users
+    .then(users => res.status(200).json(users));
 });
 
 // Server listening on port (const PORT) above
